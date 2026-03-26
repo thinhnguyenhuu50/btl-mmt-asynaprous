83d0d05c35ee8cef60d4f273778861d5105ccaf7
Thinh <thinhnguyenhuu50@gmail.com>
2026-03-26 12:50:28 +0700

    Bug fixes from the initial code
    ------------------------------------
    Format:
    File
    Issue
    Fix
    
    dictionary.py
    collections.MutableMapping deprecated in Python 3.10+	Changed to collections.abc.MutableMapping
    
    utils.py
    from urlparse Python 2 syntax	Changed to from urllib.parse
    
    init
    .py
    Wrong import path app.sampleapp	Fixed to apps.sampleapp
    
    request.py
    Crash: self.headers accessed before assignment, typo _raw_heaers	Complete rewrite with proper initialization

--- daemon/request.py
+++ daemon/request.py
@@ -18,6 +18,7 @@ This module provides a Request object to manage and persist
 request settings (cookies, auth, proxies).
 """
 from .dictionary import CaseInsensitiveDict
+import base64
 
 class Request():
     """The fully mutable "class" `Request <Request>` object,
@@ -56,11 +57,11 @@ class Request():
         #: HTTP URL to send the request to.
         self.url = None
         #: dictionary of HTTP headers.
-        self.headers = None
+        self.headers = {}
         #: HTTP path
         self.path = None        
         # The cookies set used to create Cookie header
-        self.cookies = None
+        self.cookies = {}
         #: request body to send to the server.
         self.body = None
         # The raw header
@@ -71,6 +72,8 @@ class Request():
         self.routes = {}
         #: Hook point for routed mapped-path
         self.hook = None
+        #: Authentication credentials (username, password)
+        self.auth = None
 
     def extract_request_line(self, request):
         try:
@@ -81,7 +84,7 @@ class Request():
             if path == '/':
                 path = '/index.html'
         except Exception:
-            return None, None
+            return None, None, None
 
         return method, path, version
              
@@ -108,61 +111,85 @@ class Request():
         """Prepares the entire request with the given parameters."""
 
         # Prepare the request line from the request header
-        print("[Request] prepare request missg {}".format(request))
+        print("[Request] prepare request msg {}".format(request[:200] if request else ""))
         self.method, self.path, self.version = self.extract_request_line(request)
         print("[Request] {} path {} version {}".format(self.method, self.path, self.version))
 
+        # Split raw headers and body
+        self._raw_headers, self._raw_body = self.fetch_headers_body(request)
+        self.body = self._raw_body
+
+        # Parse headers into dictionary
+        self.headers = self.prepare_headers(request)
+
+        # Parse cookies from Cookie header (RFC 6265)
+        cookie_str = self.headers.get('cookie', '')
+        self.cookies = {}
+        if cookie_str:
+            for pair in cookie_str.split(';'):
+                pair = pair.strip()
+                if '=' in pair:
+                    key, value = pair.split('=', 1)
+                    self.cookies[key.strip()] = value.strip()
+
+        # Parse Basic auth from Authorization header (RFC 2617/7235)
+        auth_header = self.headers.get('authorization', '')
+        if auth_header.startswith('Basic '):
+            try:
+                decoded = base64.b64decode(auth_header[6:]).decode('utf-8')
+                username, password = decoded.split(':', 1)
+                self.auth = (username, password)
+            except Exception:
+                self.auth = None
+
         #
-        # @bksysnet Preapring the webapp hook with AsynapRous instance
+        # @bksysnet Preparing the webapp hook with AsynapRous instance
         # The default behaviour with HTTP server is empty routed
         #
-        # TODO manage the webapp hook in this mounting point
-        #
-        
-        if not routes == {}:
+        if routes and routes != {}:
             self.routes = routes
             print("[Request] Routing METHOD {} path {}".format(self.method, self.path))
             self.hook = routes.get((self.method, self.path))
-            print("[Request] Hook has request {}".format(request))
-            #
-            # self.hook manipulation goes here
-            # ...
-            #
-
-        self._raw_heaers = ""
-        self._raw_body =  ""
-        cookies = self.headers.get('cookie', '')
-            #
-            #  TODO: implement the cookie function here
-            #        by parsing the header            #
+            print("[Request] Hook has request {}".format(self.hook))
 
         return
 
     def prepare_body(self, data, files, json=None):
+        if data:
+            self.body = data
         self.prepare_content_length(self.body)
-        self.body = body
-        #
-        # TODO prepare the request authentication
-        #
-	# self.auth = ...
         return
-
 
     def prepare_content_length(self, body):
-        self.headers["Content-Length"] = "0"
-        #
-        # TODO prepare the request authentication
-        #
-	# self.auth = ...
+        if body:
+            self.headers["Content-Length"] = str(len(body))
+        else:
+            self.headers["Content-Length"] = "0"
         return
 
-
     def prepare_auth(self, auth, url=""):
-        #
-        # TODO prepare the request authentication
-        #
-	# self.auth = ...
+        """Prepare the request authentication.
+
+        :param auth: tuple of (username, password)
+        :param url: optional URL
+        """
+        if auth and len(auth) == 2:
+            credentials = base64.b64encode(
+                "{}:{}".format(auth[0], auth[1]).encode('utf-8')
+            ).decode('utf-8')
+            self.headers["Authorization"] = "Basic {}".format(credentials)
+            self.auth = auth
         return
 
     def prepare_cookies(self, cookies):
+        """Prepare the Cookie header from a cookies dictionary.
+
+        :param cookies: dict of cookie key-value pairs
+        """
+        if isinstance(cookies, dict):
+            cookie_str = '; '.join(
+                '{}={}'.format(k, v) for k, v in cookies.items()
+            )
+            self.headers["Cookie"] = cookie_str
+        elif isinstance(cookies, str):
             self.headers["Cookie"] = cookies
